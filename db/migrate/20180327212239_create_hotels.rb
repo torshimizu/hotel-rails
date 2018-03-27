6206e2845f45e856436b897d1d6ccdaead67cced
Tor Shimizu <torshimizu@gmail.com>
2018-03-27 14:29:42 -0700

    inital hotel schema

--- db/migrate/20180327212239_create_hotels.rb
+++ db/migrate/20180327212239_create_hotels.rb
@@ -1,6 +1,8 @@
 class CreateHotels < ActiveRecord::Migration[5.1]
   def change
     create_table :hotels do |t|
+      t.string :hotel_name
+      t.integer :number_of_rooms
 
       t.timestamps
     end
