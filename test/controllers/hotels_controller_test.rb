02d1839636f8a188d38d26d248936588e138eddd
Tor Shimizu <torshimizu@gmail.com>
2018-03-27 18:52:02 -0700

    removed the hotel controller for now

--- test/controllers/hotels_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-describe HotelsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
-end
--- test/models/hotel_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe Hotel do
-  let(:hotel) { Hotel.new }
-
-  it "must be valid" do
-    value(hotel).must_be :valid?
-  end
-end
