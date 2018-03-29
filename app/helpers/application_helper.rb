a793b80aa8e01e843de9b4c64d89133778ffdca5
Tor Shimizu <torshimizu@gmail.com>
2018-03-28 21:28:56 -0700

    added an admin controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+  def parse(date)
+    case
+    when date.instance_of?(Date)
+      return date
+    when date.match(/^\d{2,4}-\d{1,2}-\d{1,2}$/)
+      Date.parse(date)
+    when date.match(/^\d{1,2}-\d{1,2}-\d{4}$/)
+      Date.strptime(date, '%m-%d-%Y')
+    when date.match(/^\d{1,2}\/\d{1,2}\/\d{4}$/)
+      Date.strptime(date, '%m/%d/%Y')
+    when date.match(/^\d{6,8}$/)
+      Date.parse(date)
+    end
+  end
+
+  def overlap_date_range?(start_date, end_date, block_res)
+    reservation_range = (block_res.start_date...block_res.end_date).to_a
+    check_range = (parse(start_date)...parse(end_date)).to_a
+    overlap = reservation_range & check_range
+    return !overlap.empty?
+  end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,18 @@
 class Room < ApplicationRecord
   has_many :reservations
+
+  def check_availability(start_date, end_date)
+    start_date = parse(start_date)
+    end_date = parse(end_date)
+
+    # if there already is a reservation, then this room is not available
+    @reservations.each do |reservation|
+      if overlap_date_range?(start_date, end_date, reservation)
+        return :UNAVAILABLE
+      end
+    end
+    return :AVAILABLE
+
+  end
+
 end
