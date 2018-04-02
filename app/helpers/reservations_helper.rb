4421d6213e2ae6d80e5b41e76f82e6801629717c
Tor Shimizu <torshimizu@gmail.com>
2018-04-01 23:08:01 -0700

    added a navbar icon, changed rooms list on root to dropdown, more css formatting

--- app/helpers/reservations_helper.rb
+++ app/helpers/reservations_helper.rb
@@ -1,13 +1,9 @@
 module ReservationsHelper
-  def get_available_room(start_date, end_date)
-    return find_available_rooms(start_date, end_date).first
-  end
-
-  def find_available_rooms(start_date, end_date) # this maybe should not be here as Reservations should not know about Room
+  def get_available_room(start_date, end_date) # this maybe should not be here as Reservations should not know about Room
     rooms = Room.all
     available_rooms = rooms.select { |room| room.check_availability(start_date, end_date) == :AVAILABLE }
 
-    return available_rooms
+    return available_rooms.first
   end
 
   def list_reservations(start_date)
